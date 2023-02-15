116f16a80007c51488da3d15e479d9f0b954094a
TomJB1 <tombrandisicloud@gmail.com>
2023-02-15 17:08:34 +0000

    Add evaluateWinner script
    Takes the player and computers choices (RPS) and evaluates weather the
    player wins, loses or draws

--- script.js
+++ script.js
@@ -5,15 +5,86 @@ function computerRandomChoice()
     switch(rnumb)
     {
         case 0:
-            choice = "Rock"
+            choice = "Rock";
         break;
 
         case 1:
-            choice = "Paper"
+            choice = "Paper";
         break;
 
         case 2:
-            choice = "Scissors"
+            choice = "Scissors";
         break;
+
     }
+
+    return choice
+
 }
+
+function getInput()
+{
+    playerchoice = prompt("enter")
+
+    return playerchoice
+}
+
+function evaluateWinner(player, computer)
+{
+    if (player == "Rock")
+    {
+        switch(computer)
+        {
+            case "Rock":
+                outcome = "draw";
+            break;
+
+            case "Paper":
+                outcome = "lose";
+            break;
+
+            case "Scissors":
+                outcome = "win";
+            break;
+        }
+    }
+
+    if (player == "Paper")
+    {
+        switch(computer)
+        {
+            case "Rock":
+                outcome = "win";
+            break;
+
+            case "Paper":
+                outcome = "draw";
+            break;
+
+            case "Scissors":
+                outcome = "lose";
+            break;
+        }
+    }
+
+    if (player == "Scissors")
+    {
+        switch(computer)
+        {
+            case "Rock":
+                outcome = "lose";
+            break;
+
+            case "Paper":
+                outcome = "win";
+            break;
+
+            case "Scissors":
+                outcome = "draw";
+            break;
+        }
+    }
+
+    return outcome
+}
+
